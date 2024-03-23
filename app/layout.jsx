de34d5f3237da1f59cf9555e42f2ef72c96005fc
gyanendra19 <gyanendrav416@gmail.com>
2024-03-24 00:25:31 +0530

    Initial Commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import Provider from '@components/Provider'
 import Nav from '@components/nav'
 import '@styles/global.css'
+import { Suspense } from 'react'
+
 
 export const metadata = {
     title: 'Toolsy',
@@ -15,7 +17,9 @@ const layout = ({children}) => {
             <div className='bg-[#1A1C2E] w-full min-h-screen'>
             <div className="w-1/6 h-[30px] bg-white absolute top-40 left-1/2 blur-[110px]"></div>
                 <Nav />
+                <Suspense>
                 {children}
+                </Suspense>
             </div>
           </Provider>
         </body>
